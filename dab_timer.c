b5d834b49f3f8ac09946113e84ed70132745dd8a
RogueMaster <RogueMaster@users.noreply.github.com>
2024-07-06 06:17:55 -0400

    See ROGUE-MASTER.NET for the BEST FLIPPER FIRMWARE! Works with OFW! v2.0

--- dab_timer.c
+++ dab_timer.c
@@ -72,8 +72,9 @@ void dab_timer_render_binary_face(
     }
 }
 
-static void dab_timer_input_callback(InputEvent* input_event, FuriMessageQueue* event_queue) {
-    furi_assert(event_queue);
+static void dab_timer_input_callback(InputEvent* input_event, void* ctx) {
+    furi_assert(ctx);
+    FuriMessageQueue* event_queue = ctx;
     PluginEvent event = {.type = EventTypeKey, .input = *input_event};
     furi_message_queue_put(event_queue, &event, FuriWaitForever);
 }
